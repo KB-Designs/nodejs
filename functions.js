208b5a83b0d13ef4c0afd836021047a43f5fabfe
Brian <kimanibrian030@gmail.com>
2025-10-01 23:58:00 +0300

    .

---
+++ functions.js
@@ -0,0 +1,7 @@
+function product(num1,num2){
+    return num1*num2;
+}
+
+let result = product(2,3);
+
+console.log(result);
---
+++ variables.js
@@ -0,0 +1,37 @@
+let x = 5;
+
+{
+let x  = 10;
+//console.log(x);
+}
+
+
+
+
+var y = 20;
+var y = 30;
+
+let sum =x+y;
+//console.log(sum);
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+let cars=["bmw","audi","ferrari","mercedes","toyota"];
+
+let len=cars.length;
+for (let i=0; i<len; i++){
+    console.log(cars[i]);
+}
+
+let num=[1,2,3,4,5,6,7,8,9,10];
+let lenth=num.length;
+
+for (let i =0; i<11; i++){
+    
+
+}
+
+
